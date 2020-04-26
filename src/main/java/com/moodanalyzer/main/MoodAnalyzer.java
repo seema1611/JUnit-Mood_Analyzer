a29a0a76223c8a507619831b223ab69e42ae1e7e
seema1611 <seemarajpure16@gmail.com>
2020-04-26 12:29:05 +0530

    REFACTORED:[TC-1.1] Constructor to take input, check sad mood

--- src/main/java/com/moodanalyzer/main/MoodAnalyzer.java
+++ src/main/java/com/moodanalyzer/main/MoodAnalyzer.java
@@ -1,11 +1,30 @@
 package com.moodanalyzer.main;
 
 public class MoodAnalyzer {
+    String message;
 
-    public static String analyzeMood(String message) {
+    //Default Constructor
+    public MoodAnalyzer()
+    {
+
+    }
+
+    //Parameterized constructor and pass string parameter
+    public MoodAnalyzer(String message)
+    {
+        this.message=message;
+    }
+
+    //Paramerized method
+    public String analyzeMood(String message) {
         if (message.contains("Sad"))
             return "Sad";
         else
             return "Happy";
+    }
+
+
+    public String analyzeMood() {
+        return analyzeMood(this.message);
     }
 }
--- src/test/java/com/moodanalyzer/test/MoodAnalyzerTest.java
+++ src/test/java/com/moodanalyzer/test/MoodAnalyzerTest.java
@@ -6,15 +6,24 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
 
+    //TC-1.1
     @Test
     public void givenMood_WhenSadMessage_ShouldReturnSad() {
         MoodAnalyzer analyzer = new MoodAnalyzer();
         Assert.assertEquals("Sad",analyzer.analyzeMood("I Am In Sad Mood"));
     }
 
+    //TC-1.2
     @Test
     public void givenMood_WhenAnyMessage_ShouldReturnHappy() {
         MoodAnalyzer analyzer = new MoodAnalyzer();
         Assert.assertEquals("Happy",analyzer.analyzeMood("I Am In Any Mood"));
+    }
+
+    //REFACTORED-1.1
+    @Test
+    public void givenMood_WhenSadMessageConstructor_ShouldReturnSad() {
+        MoodAnalyzer analyzer = new MoodAnalyzer("I Am In Sad Mood");
+        Assert.assertEquals("Sad",analyzer.analyzeMood());
     }
 }
